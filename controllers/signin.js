4818dcd60ea4d78754d7c3f690365f71461c8184
Abiola <ajibolaabiolaahmed@gmail.com>
2020-07-25 20:34:25 +0100

    Add a captcha failure response

--- controllers/signin.js
+++ controllers/signin.js
@@ -14,39 +14,67 @@ const handleSignin = (request, response, knex, bcrypt, validatePW, validateEm) =
     .then(res => res.json())
     .then(resObj => {
         console.log(resObj)
-        success = resObj.success;
+        // success = resObj.success;
+        if ((validateEm(email) && validatePW(password))) {
+            console.log('mail', validateEm(email))
+            console.log('pw', validatePW(password))
+            return response.status(400).json('Not allowed')
+        } else if (!resObj.success) {
+            response.status(400).json('Error input');
+        } else {
+            knex('login').where({
+                email: email
+            }).select('email', 'hash')
+                .then(data => {
+                    // console.log(data[0])
+                    bcrypt.compare(password, data[0].hash, function (err, res) {
+                        if (res) {
+                            return knex('users').where('email', email)
+                                .then(user => {
+                                    console.log(user[0])
+                                    console.log('found')
+                                    response.json(Object.assign({}, user[0], { comment: "good request" }))
+                                })
+                                .catch((e) => console.error('user not found', e))
+                        } else {
+                            response.status(400).json('Passed1: Incorrect credentials')
+                        }
+                    })
+                })
+                .catch(() => response.status(400).json('Passed2: Incorrect credentials'))
+        }
     })
     .catch(console.log)
     ////////////////////////////
 
-    if ((validateEm(email) && validatePW(password))) {
-        console.log('mail', validateEm(email))
-        console.log('pw', validatePW(password))
-        return response.status(400).json('Not allowed')
-    }else if (!success) {
-        response.status(400).json('Error input');
-    } else {
-        knex('login').where({
-            email: email
-        }).select('email', 'hash')
-            .then(data => {
-                // console.log(data[0])
-                bcrypt.compare(password, data[0].hash, function (err, res) {
-                    if (res) {
-                        return knex('users').where('email', email)
-                            .then(user => {
-                                console.log(user[0])
-                                console.log('found')
-                                response.json(Object.assign({}, user[0], { comment: "good request" }))
-                            })
-                            .catch((e) => console.error('user not found', e))
-                    } else {
-                        response.status(400).json('Passed1: Incorrect credentials')
-                    }
-                })
-            })
-            .catch(() => response.status(400).json('Passed2: Incorrect credentials'))
-    }
+    // if ((validateEm(email) && validatePW(password))) {
+    //     console.log('mail', validateEm(email))
+    //     console.log('pw', validatePW(password))
+    //     return response.status(400).json('Not allowed')
+    // }else if (!success) {
+    //     response.status(400).json('Error input');
+    // } else {
+    //     knex('login').where({
+    //         email: email
+    //     }).select('email', 'hash')
+    //         .then(data => {
+    //             // console.log(data[0])
+    //             bcrypt.compare(password, data[0].hash, function (err, res) {
+    //                 if (res) {
+    //                     return knex('users').where('email', email)
+    //                         .then(user => {
+    //                             console.log(user[0])
+    //                             console.log('found')
+    //                             response.json(Object.assign({}, user[0], { comment: "good request" }))
+    //                         })
+    //                         .catch((e) => console.error('user not found', e))
+    //                 } else {
+    //                     response.status(400).json('Passed1: Incorrect credentials')
+    //                 }
+    //             })
+    //         })
+    //         .catch(() => response.status(400).json('Passed2: Incorrect credentials'))
+    // }
 }
 
 module.exports = {
