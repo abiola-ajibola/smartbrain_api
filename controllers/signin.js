12272f13a437e9b1eff39f24dad9e48632dd2dae
Abiola <ajibolaabiolaahmed@gmail.com>
2021-01-22 16:19:36 +0100

    rectified axios security vulnerability using npm

--- controllers/signin.js
+++ controllers/signin.js
@@ -7,7 +7,6 @@ const handleSignin = (request, response, knex, bcrypt, validatePW, validateEm) =
     console.log('Password', password, validatePW(password));
     console.log(recaptcha);
 
-    ///////////////////////////////
     fetch(`https://www.google.com/recaptcha/api/siteverify?secret=6LdjSrIZAAAAAGbAFZOMiL_vZMoasCCBvJoN_HIq&response=${recaptcha}`,{
         method: 'POST'
     })
@@ -45,36 +44,6 @@ const handleSignin = (request, response, knex, bcrypt, validatePW, validateEm) =
         }
     })
     .catch(console.log)
-    ////////////////////////////
-
-    // if ((validateEm(email) && validatePW(password))) {
-    //     console.log('mail', validateEm(email))
-    //     console.log('pw', validatePW(password))
-    //     return response.status(400).json('Not allowed')
-    // }else if (!success) {
-    //     response.status(400).json('Error input');
-    // } else {
-    //     knex('login').where({
-    //         email: email
-    //     }).select('email', 'hash')
-    //         .then(data => {
-    //             // console.log(data[0])
-    //             bcrypt.compare(password, data[0].hash, function (err, res) {
-    //                 if (res) {
-    //                     return knex('users').where('email', email)
-    //                         .then(user => {
-    //                             console.log(user[0])
-    //                             console.log('found')
-    //                             response.json(Object.assign({}, user[0], { comment: "good request" }))
-    //                         })
-    //                         .catch((e) => console.error('user not found', e))
-    //                 } else {
-    //                     response.status(400).json('Passed1: Incorrect credentials')
-    //                 }
-    //             })
-    //         })
-    //         .catch(() => response.status(400).json('Passed2: Incorrect credentials'))
-    // }
 }
 
 module.exports = {
